8a6be8bb1ecde2e6e2990231e64f0bd3a82c48d9
maat <maat@ixiaopu.com>
2017-04-19 12:59:10 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import clipboard from './clipboard';
+
+const ReactClipBoard = React.createClass({
+
+  propTypes: {
+    text: PropTypes.string.isRequired,
+    children: PropTypes.element.isRequired,
+    onCopy: PropTypes.func,
+    onFail: PropTypes.func
+  },
+
+  onClick(event) {
+    const {text, onCopy, onFail} = this.props;
+    clipboard.copy(text, function(){
+      onCopy && onCopy();
+    }, function () {
+      onFail && onFail();
+    });
+  },
+
+  componentWillUnmount () {
+    clipboard.destroy();
+  },
+  
+  render () {
+    const {
+      text: _text,
+      onCopy: _onCopy,
+      onFail, _onFail,
+      options: _options,
+      children,
+      ...props
+    } = this.props;
+    const elem = React.Children.only(children);
+
+    return React.cloneElement(elem, {...props, onClick: this.onClick.bind(this)});
+  }
+});
+
+export default ReactClipBoard;
+
